fdcf8372ffc95e55105ee76f1d2c5c7ebefc7f0b
NuubieDevelopment <nuubiedevelopment@gmail.com>
2025-02-22 10:52:49 -0300

    style: css divided into parts

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: [
+    './introduction.component.css',
+    './about.component.css',
+    './benefits.component.css',
+    './video.component.css',
+    './forms.component.css',
+    './how_works.component.css',
+    './footer.component.css',
+  ]
 })
 export class HomeComponent {
 
